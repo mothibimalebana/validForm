ccf0eff2f82ef050329c16583a5927871ab565dc
mothibi.malebana <100524435+mothibimalebana@users.noreply.github.com>
2024-12-09 17:46:26 +0200

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/js/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'App Name',
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
